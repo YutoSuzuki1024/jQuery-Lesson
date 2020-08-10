3f0461af326cefbae245c946553dd7cf53778311
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-08-10 18:46:04 +0900

    Finish JS7

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-// $(function(){
-// 	$(".box1").slideDown();
-// 	$(".box1").css({
-// 		"width": "200px",
-// 		"height": "100px",
-// 		"background-color": "blue"
-// 	});
-// 	$(".box1").slideUp();
-// });
-
-// $(function(){
-//     $('.box1').slideDown(function() {
-//         $('.box1').css({
-//             'background-color': '#0000FF',
-//             'width': '200px',
-//             'height': '100px'
-//         })
-//         .slideUp();
-//     });
-// });
-
-$(function () {
-	$(".box1").fadeOut(1000);
+$(function(){
+	$("button").on("click", function(){
+		$("ul").children().css("color", "red");
+	});
 });
