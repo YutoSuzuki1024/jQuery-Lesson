00dd53be9f7feb007a57191af8a4a6fc64dd38b2
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-08-10 17:15:44 +0900

    Finish JS5

---
+++ app.js
@@ -0,0 +1,24 @@
+// $(function(){
+// 	$(".box1").slideDown();
+// 	$(".box1").css({
+// 		"width": "200px",
+// 		"height": "100px",
+// 		"background-color": "blue"
+// 	});
+// 	$(".box1").slideUp();
+// });
+
+// $(function(){
+//     $('.box1').slideDown(function() {
+//         $('.box1').css({
+//             'background-color': '#0000FF',
+//             'width': '200px',
+//             'height': '100px'
+//         })
+//         .slideUp();
+//     });
+// });
+
+$(function () {
+	$(".box1").fadeOut(1000);
+});
